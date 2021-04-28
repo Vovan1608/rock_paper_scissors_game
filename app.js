8420fb27901133913b13fad2d4d8ec68fe54e61c
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-28 13:13:08 +0300

    added variables

--- app.js
+++ app.js
@@ -3,3 +3,12 @@
 const userScore = 0;
 const machineScore = 0;
 
+const userScoreDOM = document.getElementById("user-score");
+const machineScoreDOM = document.getElementById("computer-score");
+const scoreBoard = document.querySelector(".score-board");
+const result = document.querySelector(".result");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+// listeners
